d8b6b82e024a016f544ed6383bd8f90b9da662d5
zxc8278 <55608306+zxc8278@users.noreply.github.com>
2022-03-29 22:02:57 -0400

    Update Q routing.py

--- Q routing.py
+++ Q routing.py
@@ -8,161 +8,54 @@ MAX_LOOPS = 100
 GAMMA = 0.9
 EPSILON = 0.01
 
-class SARSA(object):
-    def __init__(self, world, alpha, source, dest):
-        self.world = world
-        dest = dest.split(",")
-        self.world.map[int(dest[0]), int(dest[1])] = -1
-        print(self.world.map)
-        self.source = source
-        self.alpha = alpha
-        
-        self.q_func = {s: {"up": len(world.states)/400,
-                      "down": len(world.states)/400,
-                      "left": len(world.states)/400,
-                      "right": len(world.states)/400} for s in world.states}
-        self.policy = {s: np.random.choice(world.actions) for s in world.states}
-        self.values = {s: 0.0 for s in world.states}
-        self.q_func["goal"] = {"up": 0, "down": 0, "left": 0, "right": 0}
-        
-        self.hist = []
-        self.rewards = []
-        self.actions = []
-        self.states = []
-    
-    def __call__(self):
-        s = self.source #np.random.choice(self.world.states) #self.source # P [s0, a0] > 0 for all s in STATES, a in ACTIONS
-        
-        # generate episode using policy
-        #while s != "goal":
-        if np.random.random() <= EPSILON: # e-greedy SARSA
-            # explore
-            a = np.random.choice(self.world.actions)
+class Q_routing(object):
+     def __init__(self, num_nodes, num_actions, distance, nlinks):
+        self.config = {
+            "init_mean" : 0.0,      # Initialize Q values with this mean
+            "init_std" : 0.0,       # Initialize Q values with this standard deviation
+            "learning_rate" : 0.7,
+            "eps": 0.1,            # Epsilon in epsilon greedy policies
+            "discount": 1,
+            "n_iter": 10000000}        # Number of iterations
+        self.q = np.zeros((num_nodes,num_nodes,num_actions))
+
+        for src in range(num_nodes):
+            for dest in range(num_nodes):
+                for action in range(nlinks[src]):
+                    self.q[src][dest][action] = distance[src][dest]
+
+    def act(self, state, nlinks,  best=False):
+        n = state[0]
+        dest = state[1]
+
+        if best is True:
+            best = self.q[n][dest][0]
+            best_action = 0
+            for action in range(nlinks[n]):
+                if self.q[n][dest][action] < best:  #+ eps:
+                    best = self.q[n][dest][action]
+                    best_action = action
         else:
-            # use optimal action from policy
-            a = self.policy.get(s)
-        self.states.append(s)
-        self.actions.append(a)
-        s_new, r = self.world.sar_dynamics(s, a)
-        self.rewards.append(r)
-        
-        if np.random.random() <= EPSILON: # e-greedy SARSA
-            # explore
-            a_new = np.random.choice(self.world.actions)
-        else:
-            a_new = self.policy.get(s)
-                
-        #print(s, s_new, a, a_new)
-        # now in s_new, it is an obstacle in other worlds now so remove it.
-        # s is available again so add it back to the other worlds.
+            best_action = int(np.random.choice((0.0, nlinks[n])))
 
-        self.q_func[s][a] = self.q_func[s][a] + self.alpha*(r + GAMMA*self.q_func[s_new][a_new] - self.q_func[s][a])
-        q_list = [self.q_func[s]["up"], self.q_func[s]["down"], self.q_func[s]["left"], self.q_func[s]["right"]]
-        optimal_action = np.argmax(q_list)
-        self.policy[s] = {0:"up", 1:"down", 2:"left", 3:"right"}.get(optimal_action)
-        self.values[s] = q_list[optimal_action]
-        #print(self.policy[s])
-        s_prev = s.split(",")
-        s_current = s_new.split(",")
-        s = s_new
-        a = a_new
-        self.hist.append(copy.copy(self.q_func))
+        return best_action
 
-        return s, [int(x) for x in s_current], [int(y) for y in s_prev]
-            
-def plot_policy(world, pol, values, filename, method_title="Dynamic Programming"):
-    max_val = np.max([v for v in values.values()])
-    min_val = np.min([v for v in values.values()])
-    if min_val == max_val:
-        values_norm = {k: 0.0 for k in values.keys()}
-    else:
-        values_norm = {k: (v - min_val)/(max_val - min_val) for k, v in values.items()}
 
-    x_pos = []
-    y_pos = []
-    color = []
-    arrow = []
-    dir = {"up": 0.0, "down": np.pi, "right": -np.pi/2.0, "left": np.pi/2.0}
+    def learn(self, current_event, next_event, reward, action, done, nlinks):
 
-    n_feat = 0
-    for i in range(world.map.shape[0]):
-        for j in range(world.map.shape[1]):
-            if world.map[i, j] == 1:
-                x_pos.append(f"{j}")
-                y_pos.append(f"{i}")
-                color.append("#FF0000")
-                n_feat += 1
-            elif world.map[i, j] == -1:
-                print("found goal")
-                x_pos.append(f"{j}")
-                y_pos.append(f"{i}")
-                color.append("#00FF00")
-                n_feat += 1
+        n = current_event[0]
+        dest = current_event[1]
 
-    for s in world.states:
-        if s == "goal":
-            continue
-        i, j = [int(v) for v in s.split(",")]
-        x_pos.append(f"{j}")
-        y_pos.append(f"{i}")
-        color.append(f"#0000FF{format(int(255*values_norm[s]), '02X')}")
-        arrow.append(dir[pol[s]])
+        n_next = next_event[0]
+        dest_next = next_event[1]
 
-    fig = figure(
-        title=f"{method_title}: GridWorld Policy and Values",
-        x_range=[f"{i}" for i in np.arange(world.map.shape[0])],
-        y_range=[f"{i}" for i in np.flip(np.arange(world.map.shape[1]))]
-    )
-    fig.rect(x_pos, y_pos, color=color, width=1, height=1)
-    fig.triangle(x_pos[n_feat:], y_pos[n_feat:], angle=arrow, size=400/np.sum(world.map.shape), color="#FF0000", alpha=0.5)
-     
-    save(fig, filename)
-    show(fig)
+        future = self.q[n_next][dest][0]
+        for link in range(nlinks[n_next]):
+            if self.q[n_next][dest][link] < future:
+                future = self.q[n_next][dest][link]
 
-def plot_values(world, values, filename):
-    max_val = np.max([v for v in values.values()])
-    min_val = np.min([v for v in values.values()])
-    if min_val == max_val:
-        values_norm = {k: 0.0 for k in values.keys()}
-    else:
-        values_norm = {k: (v - min_val)/(max_val - min_val) for k, v in values.items()}
-
-    x_pos = []
-    y_pos = []
-    color = []
-
-    n_feat = 0
-    for i in range(world.map.shape[0]):
-        for j in range(world.map.shape[1]):
-            if world.map[i, j] == 1:
-                x_pos.append(f"{j}")
-                y_pos.append(f"{i}")
-                color.append("#FF0000")
-                n_feat += 1
-            elif world.map[i, j] == -1:
-                x_pos.append(f"{j}")
-                y_pos.append(f"{i}")
-                color.append("#00FF00")
-                n_feat += 1 
-
-    for s in world.states:
-        if s == "goal":
-            continue
-        i, j = [int(v) for v in s.split(",")]
-        x_pos.append(f"{j}")
-        y_pos.append(f"{i}")
-        color.append(f"#0000FF{format(int(255*values_norm[s]), '02X')}")
-
-    fig = figure(
-        title="Dynamic Programing: GridWorld Values",
-        x_range=[f"{i}" for i in np.arange(world.map.shape[0])],
-        y_range=[f"{i}" for i in np.flip(np.arange(world.map.shape[1]))]
-    )
-    fig.rect(x_pos, y_pos, color=color, width=1, height=1)
-
-    save(fig, filename)
-    show(fig)
-
+        #Q learning
+        self.q[n][dest][action] += (reward + self.config["discount"]*future - self.q[n][dest][action])* self.config["learning_rate"]
 
 def main():
     # world = GridWorld(ROWS, COLS, goals=GOALS, obs=OBS)
