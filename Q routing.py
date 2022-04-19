a7579ac291e9da3c0c5ada1d14c873fc190081db
zxc8278 <55608306+zxc8278@users.noreply.github.com>
2022-04-19 08:20:18 -0400

    Update Q routing.py

--- Q routing.py
+++ Q routing.py
@@ -1,85 +1,112 @@
+import NetworkMdp
+import TrafficGenerator
 import numpy as np
-from bokeh.plotting import figure, save, show
-import copy
-from finitemdp import GridWorld
+import random
 
-N_EPISODES = 1000
-MAX_LOOPS = 100
-GAMMA = 0.9
-EPSILON = 0.01
+Alpha = 0.9
+EPSILON = 0.05
+Clambda = 0.9
 
-class Q_routing(object):
-     def __init__(self, num_nodes, num_actions, distance, nlinks):
-        self.config = {
-            "init_mean" : 0.0,      # Initialize Q values with this mean
-            "init_std" : 0.0,       # Initialize Q values with this standard deviation
-            "learning_rate" : 0.7,
-            "eps": 0.1,            # Epsilon in epsilon greedy policies
-            "discount": 1,
-            "n_iter": 10000000}        # Number of iterations
-        self.q = np.zeros((num_nodes,num_nodes,num_actions))
+class Q_routing:
+     def __init__(self, mapfile, alpha, EPSILON, Clambda):
+          
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.confidence_based = confidence_based
+        self.clambda = clambda
+          
+        self.q = 10000 * np.ones((self.network.nodes.shape[0], self.network.nodes.shape[1], len(NetworkMdp.actions)))
 
-        for src in range(num_nodes):
-            for dest in range(num_nodes):
-                for action in range(nlinks[src]):
-                    self.q[src][dest][action] = distance[src][dest]
+    def send_packet(self, origin, max_hops=100):
+        current_node = origin
+        action = self.get_action(current_node)
 
-    def act(self, state, nlinks,  best=False):
-        n = state[0]
-        dest = state[1]
+        hops = 0
+        nodes = []
+        while self.network.node(current_node) != NetworkMdp.DESTINATION:
+            next_node, reward = self.network.next_node(current_node, NetworkMdp.actions[action])
+            next_action = self.get_action(next_node)
 
-        if best is True:
-            best = self.q[n][dest][0]
-            best_action = 0
-            for action in range(nlinks[n]):
-                if self.q[n][dest][action] < best:  #+ eps:
-                    best = self.q[n][dest][action]
-                    best_action = action
+            q_current = self.q[current_node[0]][current_node[1]][action]
+            q_next = self.best_q(next_node)
+
+            new_q = q_current + self.alpha * (q_next - q_current)
+
+            self.q[current_node[0]][current_node[1]][action] = new_q
+            self.network.set_policy(current_node, action)
+
+            nodes.append((current_node, action))
+            current_node = next_node
+            action = next_action
+
+            hops = hops + 1
+
+            if hops > max_hops:
+                break
+
+        time = hops
+        for node, action in nodes:
+            old_estimate = self.q[node[0]][node[1]][action]
+            
+            self.q[node[0]][node[1]][action] = time
+            
+            if self.confidence_based:
+                # update confidence levels
+                if (time > old_estimate):
+                    # decrease confidence, limited to 0.0
+                    self.c[node[0]][node[1]][action] *= 0.9
+                if (time <= old_estimate):
+                    # increase confidence, limited to 1.0
+                    self.c[node[0]][node[1]][action] *= 1.1
+                    if self.c[node[0]][node[1]][action] > 1:
+                        self.c[node[0]][node[1]][action] = 1.0
+                for a in NetworkMdp.actions.keys():
+                    self.c[node[0]][node[1]][a] *= self.clambda # discount factor
+            time = time - 1
+
+        return hops
+
+    def get_action(self, node):
+        if random.uniform(0, 1) <= self.epsilon:
+            # Randomly return an action
+            return random.randint(0, len(NetworkMdp.actions) - 1)
+
+        best_q = float('inf')
+        best_actions = []
+        for action in NetworkMdp.actions.keys():
+            if self.confidence_based:
+                q = self.q[node[0]][node[1]][action]*(2-self.c[node[0]][node[1]][action])
+            else:
+                q = self.q[node[0]][node[1]][action]
+
+            if q < best_q:
+                best_q = q
+                best_actions.clear()
+                best_actions.append(action)
+            elif q == best_q:
+                best_actions.append(action)
+
+        if len(best_actions) == 0:
+            # No actions found that maximize the Q function, randomly pick one
+            return np.random.randint(0, len(NetworkMdp.actions) - 1)
         else:
-            best_action = int(np.random.choice((0.0, nlinks[n])))
+            # Randomly pick an action from the best actions that maximized the Q function
+            return best_actions[np.random.randint(0, len(best_actions))]
 
-        return best_action
+    def best_q(self, node):
+        best_q = float('inf')
+        for action in NetworkMdp.actions.keys():
+            q = self.q[node[0]][node[1]][action]
 
+            if q < best_q:
+                best_q = q
 
-    def learn(self, current_event, next_event, reward, action, done, nlinks):
-
-        n = current_event[0]
-        dest = current_event[1]
-
-        n_next = next_event[0]
-        dest_next = next_event[1]
-
-        future = self.q[n_next][dest][0]
-        for link in range(nlinks[n_next]):
-            if self.q[n_next][dest][link] < future:
-                future = self.q[n_next][dest][link]
-
-        #Q learning
-        self.q[n][dest][action] += (reward + self.config["discount"]*future - self.q[n][dest][action])* self.config["learning_rate"]
+        return best_q
 
 def main():
-    # world = GridWorld(ROWS, COLS, goals=GOALS, obs=OBS)
-    # world.save_map("map.txt")
-    world = GridWorld.from_file("mesh4x4.txt")
-    source = "0,0"
-    dest = "3,2"
-    p1_solver = SARSA(world, 0.01, source, dest)
-    p2_solver = SARSA(world, 0.01, "1,0", "2,2")
-    s = None
-    cnt = 0
-    for _ in range(N_EPISODES):
-        while s != "goal" and cnt < 16:
-            s, p1_state, prev_p1s = p1_solver()
-            print(p1_state, type(p1_state))
-            p2_solver.world.map[p1_state[0], p1_state[1]] = 1 # p1 state is an obstacle for p2
-            p2_solver.world.map[prev_p1s[0], p1_state[1]] = 0 # clear the old state
-            s, p2_state, prev_p2s = p2_solver()
-            p1_solver.world.map[p2_state[0], p2_state[1]] = 1
-            p1_solver.world.map[prev_p2s[0], p2_state[1]] = 0
-            cnt += 1
-            
-    plot_policy(p1_solver.world, p1_solver.policy, p1_solver.values, "p1sarsa_final.html", "SARSA")
-    plot_policy(p2_solver.world, p2_solver.policy, p2_solver.values, "p2sarsa_final.html", "SARSA")
+     qroute = Q_Routing("mesh4x4.txt")
+     traffic = TrafficGenerator.TrafficGenerator(qroute)
+     traffic.simulate(10000, "Q_rounting", True)
     
   
 if __name__ == "__main__":
